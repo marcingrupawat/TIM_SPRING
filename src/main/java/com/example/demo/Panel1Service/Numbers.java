2269a513b3ddd9a35cdf7fefff1a5090bbccc79b
Dell <marcin.grupawat@gmail.com>
2018-08-23 23:16:20 +0200

    i

---
+++ src/main/java/com/example/demo/Panel1Service/Numbers.java
@@ -0,0 +1,20 @@
+package com.example.demo.Panel1Service;
+
+public class Numbers {
+
+    private int id;
+    private int number;
+
+    public void setNumber (int number) {
+        this.number = number;
+    }
+
+    public void setIdNumber (int id) {
+        this.id = id;
+    }
+
+
+    public int getIdNumber () {
+        return id;
+    }
+}
